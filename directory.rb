8684957ebda2e13e7cf69e1e98b2fd8ce12bd192
osstechunlimited@gmail.com <ec2-user@ip-172-31-45-27.eu-west-1.compute.internal>
2018-11-16 14:38:22 +0000

    Step 7 - another method, asking user for input

--- directory.rb
+++ directory.rb
@@ -1,21 +1,32 @@
-# Put all students into an array.
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+# Request input from user.
+def input_students
+    puts "Please enter the names of students"
+    puts "To finish, just hit return twice"
+    
+    # Create an empty array.
+    students = []
+    
+    # Get the first name from user.
+    name = gets.chomp 
+    
+    # While the name is not empty, repeat this code.
+    while !name.empty? do
+      # Add the student hash (key-value pair) to the array.
+      students << {name: name, cohort: :november}
+      # students.push({name: name, cohort: :november})
+      # puts students
+      puts "Now we have #{students.count} students"
+      
+      # Get another name from user.
+      name = gets.chomp
+    end
+    # Return the array, so it can be referenced outside of this method.
+    students
+end
 
 # Print the header.
 def print_header
-  puts "The students of Villians Academy"
+  puts "The students of Villains Academy"
   puts "-------------"
 end
 
@@ -31,6 +42,7 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
