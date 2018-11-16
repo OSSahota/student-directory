4309ef7b06e10f60ac9ddf1eb8a4d23b51d93881
osstechunlimited@gmail.com <ec2-user@ip-172-31-45-27.eu-west-1.compute.internal>
2018-11-16 10:47:38 +0000

    Step 5 - using an array

--- directory.rb
+++ directory.rb
@@ -1,15 +1,26 @@
-student_count = 11
+# Put all students into an array.
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+  ]
+
+# Print the header.
 puts "The students of Villians Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-print "Overall, we have #{student_count} great students"
+
+# Print the student names, one name on each line.
+students.each do |student|
+  puts student
+end
+
+# Print the student count
+puts "Overall, we have #{students.count} great students"
