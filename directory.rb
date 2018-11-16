ace3d684b77a78d20e88dc48c2b17d8cf6ee1a16
osstechunlimited@gmail.com <ec2-user@ip-172-31-45-27.eu-west-1.compute.internal>
2018-11-16 18:35:45 +0000

    Step 8 exercise 5 - Adding more info country of birth, height

--- directory.rb
+++ directory.rb
@@ -1,23 +1,33 @@
 # Request input from user.
 def input_students
-    puts "Please enter the names of students"
+    puts "Please enter student details"
     puts "To finish, just hit return twice"
+    puts "-------------"
     
     # Create an empty array.
     students = []
     
     # Get the first name from user.
+    puts "Please enter name of student"
     name = gets.chomp 
     
     # While the name is not empty, repeat this code.
     while !name.empty? do
+      puts "Please enter country of birth"
+      country_of_birth = gets.chomp 
+
+      puts "Please enter height"
+      height = gets.chomp 
+
       # Add the student hash (key-value pair) to the array.
-      students << {name: name, cohort: :november}
-      # students.push({name: name, cohort: :november})
-      # puts students
+      students << {name: name, cohort: :november, 
+        country_of_birth: country_of_birth,
+        height: height}
+      
       puts "Now we have #{students.count} students"
       
       # Get another name from user.
+      puts "Please enter the name of student"
       name = gets.chomp
     end
     # Return the array, so it can be referenced outside of this method.
@@ -34,25 +44,18 @@ end
 def print(students)
   loop_count = 0
   while loop_count < students.count do
-    # .start_with?() method checks first character & .length method returns number of charaters in hash key :name.
-    if students[loop_count][:name].upcase.start_with?("M") && students[loop_count][:name].length < 12
+    # .start_with?() method checks first character & .length method returns 
+    # number of charaters in hash key :name.
+    if students[loop_count][:name].upcase.start_with?("M") && 
+      students[loop_count][:name].length < 12
       # if condition met so output index, key and value.
-      puts "#{loop_count + 1}. #{students[loop_count][:name]} (#{students[loop_count][:cohort]} cohort)"
+      puts "#{loop_count + 1}. #{students[loop_count][:name]}" +
+      " (#{students[loop_count][:cohort]} cohort)" +
+      " - #{students[loop_count][:country_of_birth]}" + 
+      " - #{students[loop_count][:height]}"
     end
     loop_count += 1
   end
-
-  #  each() method replaced by while loop above as requested in Step 8.4. 
-  # # Iterate through the array. The .each_with_index method allows use of
-  # # 0-based index.
-  # students.each_with_index do |student, index|
-  #   # .start_with?() method checks first character & .length method returns
-  #   # number of charaters in hash key :name.
-  #   if student[:name].upcase.start_with?("M") && student[:name].length < 12
-  #     # if condition met so output index, key and value.
-  #     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-  #   end
-  # end
 end
 
 # Print the student count
