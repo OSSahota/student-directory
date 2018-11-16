7bcb12f92000524a19af1d2f5bde7be3c8264ea3
osstechunlimited@gmail.com <ec2-user@ip-172-31-45-27.eu-west-1.compute.internal>
2018-11-16 17:27:57 +0000

    Step 8 exercise 4 - Rewrite each() method to use while loop

--- directory.rb
+++ directory.rb
@@ -32,16 +32,27 @@ end
 
 # Print the student names, one name on each line.
 def print(students)
-  # Iterate through the array. The .each_with_index method allows use of
-  # 0-based index.
-  students.each_with_index do |student, index|
-    # .start_with?() method checks first character & .length method returns
-    # number of charaters in has key :name.
-    if student[:name].upcase.start_with?("M") && student[:name].length < 12
+  loop_count = 0
+  while loop_count < students.count do
+    # .start_with?() method checks first character & .length method returns number of charaters in hash key :name.
+    if students[loop_count][:name].upcase.start_with?("M") && students[loop_count][:name].length < 12
       # if condition met so output index, key and value.
-      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{loop_count + 1}. #{students[loop_count][:name]} (#{students[loop_count][:cohort]} cohort)"
     end
+    loop_count += 1
   end
+
+  #  each() method replaced by while loop above as requested in Step 8.4. 
+  # # Iterate through the array. The .each_with_index method allows use of
+  # # 0-based index.
+  # students.each_with_index do |student, index|
+  #   # .start_with?() method checks first character & .length method returns
+  #   # number of charaters in hash key :name.
+  #   if student[:name].upcase.start_with?("M") && student[:name].length < 12
+  #     # if condition met so output index, key and value.
+  #     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  #   end
+  # end
 end
 
 # Print the student count
