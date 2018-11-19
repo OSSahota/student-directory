fdd6009ae5990420182afc2d685eb612ed5c60a6
osstechunlimited@gmail.com <ec2-user@ip-172-31-45-27.eu-west-1.compute.internal>
2018-11-19 17:42:24 +0000

    Step 10 - Refactoring code from step 9 interactive menu file.

--- interactive_menu.rb
+++ interactive_menu.rb
@@ -1,37 +1,44 @@
+# an empty array accessible to all methods (global variable)
+@students = [] 
+
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  # 1. print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+  # 2. read the input and save it into a variable
+end
+
+def process(selection)
     # 3. do what the user has asked
     case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
       exit # this will cause the program to terminate
     else
       puts "I don't know what you meant, try again"
     end
-  end
 end
 
-# This exercise involves debugging the code, stepping through each 
-# line and correcting the typos.
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  
-  # create an empty array
-  students = []
   
   # get the first name
   name = gets.chomp
@@ -39,31 +46,28 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    @students << {name: name, cohort: :november}
 
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{@students.count} students"
     
     # get another name from the user - correction applied
     name = gets.chomp
   end
-  
-  # return the array of students
-  students
 end
 
-def print_header 
+def print_header
   puts "The students of my cohort at Makers Academy"
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def print_student_list
+  @students.each do |student|
+  puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 # Call interactive_menu method.
